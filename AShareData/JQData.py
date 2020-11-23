dc9e38a0b8ea5112151557f838e659f572a8e4e8
Ji Ce <jice@yanfenginv.com>
2020-11-23 09:24:56 +0800

    add JQ stock minute

--- AShareData/JQData.py
+++ AShareData/JQData.py
@@ -8,7 +8,7 @@ from typing import Dict, List, Sequence, Union, Optional
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from jqdatasdk import auth, bond, query
+from jqdatasdk import auth, bond, query, get_price, get_call_auction
 
 
 from . import constants, utils, DateUtils
@@ -36,7 +36,11 @@ class JQData(DataSource):
 
         self._factor_param = utils.load_param('jqdata_param.json')
 
-    def update_convertable_bond_list(self):
+    @cached_property
+    def stock_tickers(self):
+        return StockTickers(self.db_interface)
+
+    def update_convertible_bond_list(self):
         q = query(bond.BOND_BASIC_INFO).filter(bond.BOND_BASIC_INFO.bond_type == '可转债')
         df = bond.run_query(q)
         exchange = df.exchange.map({'深交所主板': '.SZ', '上交所': '.SH'})
@@ -49,6 +53,17 @@ class JQData(DataSource):
         ret = df.loc[:, renaming_dict.keys()].rename(renaming_dict, axis=1).set_index('ID')
         self.db_interface.update_df(ret, '可转债信息')
         print(df)
+
+    def update_stock_minute_data(self):
+        date = dt.datetime(2020, 11, 20)
+        pre_date = date - dt.timedelta(days=1)
+        date_start = date + dt.timedelta(hours=8)
+        date_end = date + dt.timedelta(hours=16)
+        tickers = self.stock_tickers.ticker(date)
+        tickers = ['000001.XSHE']
+        data = get_price(tickers, start_date=date_start, end_date=date_end, frequency='1m', fq=None, fill_paused=True)
+
+        auction = get_call_auction(tickers, DateUtils.date_type2str(pre_date, '-'), DateUtils.date_type2str(date, '-'))
 
     @staticmethod
     def jqcode_to_windcode(jq_code: str) -> Optional[str]:
