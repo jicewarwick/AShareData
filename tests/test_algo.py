76d98f7a303d7556db958e57834e5e6038e20e19
Ji Ce <jice@yanfenginv.com>
2021-03-10 14:48:38 +0800

    [feature] 增加判断主板函数

--- tests/test_algo.py
+++ tests/test_algo.py
@@ -1,5 +1,6 @@
 import unittest
 from AShareData.algo import *
+from AShareData.utils import *
 
 
 class MyTestCase(unittest.TestCase):
@@ -14,6 +15,21 @@ class MyTestCase(unittest.TestCase):
         print(get_less_or_equal_of_a_in_b(a2, b))
         print(get_less_or_equal_of_a_in_b(a3, b))
         print(get_less_or_equal_of_a_in_b(a4, b))
+
+    def test_is_stock_ticker(self):
+        self.assertEqual(get_stock_board_name('000001.SZ'), '主板')
+        self.assertEqual(get_stock_board_name('001979.SZ'), '主板')
+        self.assertEqual(get_stock_board_name('600000.SH'), '主板')
+        self.assertEqual(get_stock_board_name('605500.SH'), '主板')
+
+        self.assertEqual(get_stock_board_name('002594.SZ'), '中小板')
+        self.assertEqual(get_stock_board_name('300498.SZ'), '创业板')
+        self.assertEqual(get_stock_board_name('688688.SH'), '科创板')
+
+        self.assertEqual(get_stock_board_name('0196.HK'), '非股票')
+        self.assertEqual(get_stock_board_name('IF1208.CFE'), '非股票')
+        self.assertEqual(get_stock_board_name('300498'), '非股票')
+        self.assertEqual(get_stock_board_name(300498), '非股票')
 
 
 if __name__ == '__main__':
